b575bd8aae4f0098eda4227c8ca2b3e9e4a42249
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 15:33:21 +0000

    Adding tarfile member sanitization to extractall()

--- go-backup.py
+++ go-backup.py
@@ -26,7 +26,26 @@ def extract_tarfile(FILE_NAME, DIR):
 
     with tarfile.open(FILE_NAME) as tar:
         print("Extraction Ongoing... Please wait...")
-        tar.extractall(DIR) #Specific folder
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, DIR)
         tar.close()
     print(f"{FILE_NAME} has been Successfully Extracted.")
 
